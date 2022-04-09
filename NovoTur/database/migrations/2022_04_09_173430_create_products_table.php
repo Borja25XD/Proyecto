4ef8f784e026a428a1afc91aa2f2d1bbd9e132c2
Jose <prueba@gmail.com>
2022-04-09 19:07:27 +0100

    Migrations con la tabla products

---
+++ NovoTur/database/migrations/2022_04_09_173430_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->string('short_name', 50)->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('category', 12);
+            $table->string('brand');
+            $table->string('url')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
