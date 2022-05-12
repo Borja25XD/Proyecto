8544237a3b2cdfc212f45f7cdb7a4c737f086eed
Borja Santos <borjasantos7@gmail.com>
2022-05-12 12:26:04 +0200

    Menu de navegación responsivo

--- Futpad/resources/views/nav.blade.php
+++ Futpad/resources/views/nav.blade.php
@@ -1,44 +1,48 @@
-<nav class="navbar-expand-md navbar-light bg-white shadow-sm ">
-    <div class="row m-0">
+<nav class="navbar-expand-md navbar-light bg-white shadow-sm d-none d-sm-none d-md-block">
+    <div class="m-0">
         <ul class="nav">
             <a href={{ route('home') }}>
                 <img class="col-12" src={{ url('/images/Futpadlogo.png') }} alt="{{ __('Logo de Futpad') }}"
                     height="80px">
             </a>
-            <li class="nav-item offset-md-1 offset-lg-3 offset-xl-4 col-1">
-                <a class="nav-link {{ setActive('shop') }}" href={{ route('shop') }}>
+            <li class="nav-item offset-md-0 offset-lg-1 offset-xl-2 col-lg-1 text-center d-inline-block">
+                <a class="nav-link customLink {{ setActive('shop') }}" href={{ route('shop') }}>
                     {{ __('Shop') }}
                 </a>
             </li>
-            <li class="nav-item col-1">
-                <a class=" nav-link {{ setActive('booking') }}" href={{ route('booking') }}>
+            <li class=" nav-item col-lg-1 text-center">
+                <a class=" nav-link customLink {{ setActive('booking') }}" href={{ route('booking') }}>
                     {{ __('Booking') }}
                 </a>
             </li>
             @guest
-                <li class="nav-item col-1"><a class="nav-link {{ setActive('register') }}"
+                <li class="nav-item col-lg-1 col-md-1 text-center "><a class="nav-link customLink {{ setActive('register') }}"
                         href={{ route('register') }}>{{ __('Register') }}</a>
                 </li>
-                <li class="nav-item col-2">
+                <li
+                    class="nav-item  mx-md-3 col-lg-1 col-md-2 offset-md-0 offset-lg-0 text-center d-inline-block text-nowrap">
                     <a class="nav-link {{ setActive('login') }}" href={{ route('login') }}>
                         {{ __('Login') }}
                     </a>
                 </li>
             @else
                 @if (auth()->user()->type == 'admin')
-                    <li class="nav-item col-1"><a class="nav-link"
+                    <li class="nav-item col-1 text-center"><a class="nav-link"
                             href={{ route('dashboard') }}>{{ __('Manage') }}</a>
                     </li>
                 @else
-                    <li class="nav-item col-1"><a class="nav-link"
+                    <li class="nav-item col-1 text-center"><a class="nav-link"
                             href={{ route('dashboard') }}>{{ __('My account') }}</a>
                     </li>
                 @endif
-                <li class="nav-item col-1"><a class="nav-link" href="#"
+                <li class="nav-item col-1 text-center text-nowrap"><a class="nav-link" href="#"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </li>
             @endguest
-            <li class="nav-item my-2 offset-1">
+            <li class="nav-item my-2 offset-md-1 offset-lg-1 offset-xl-2">
                 <div class="dropdown">
                     <a class="btn btn-secondary dropdown-toggle bg-primary" href="#" role="button" id="dropdownMenuLink"
                         data-bs-toggle="dropdown" aria-expanded="false">
@@ -62,20 +66,93 @@
                         data-bs-toggle="dropdown" aria-expanded="false">
                         <img src=" {{ url('/images/shopping-cart.png') }}" alt="shopping-cart"
                             style="max-height: 18px;">
-                        <span> ({{Cart::getContent()->count()}})</span>
+                        <span> ({{ Cart::getContent()->count() }})</span>
                     </a>
                     <ul class="dropdown-menu shopping-cart" aria-labelledby="dropdownMenuLink" id="langList">
+                        @if (Cart::getContent())
+                            <li><a class="dropdown-item"
+                                    href="{{ route('cart.checkout') }}">{{ __('View all cart') }}<span>
+                                        ({{ Cart::getContent()->count() }})</span></a></li>
+                        @endif
+                    </ul>
+                </div>
             </li>
-            @if(Cart::getContent())
-            <li><a class="dropdown-item" href="{{ route('cart.checkout') }}">{{ __('View all cart') }}<span> ({{Cart::getContent()->count()}})</span></a></li>
-            @endif
-        </ul>
-    </div>
-    </li>
-    </ul>
     </div>
 </nav>
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-    @csrf
-</form>
+
+
+<nav class="navbar navbar-dark bg-light d-block d-sm-block d-md-none">
+    <div class="d-flex">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="">
+                <img src={{ url('/images/Futpadlogo.png') }} alt="{{ __('Logo de Futpad') }}" height="80px">
+            </span>
+        </button>
+        <div class="d-flex align-items-end m-auto">
+            <div class="dropdown mx-2">
+                <a class="btn btn-secondary dropdown-toggle bg-primary" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ __('Language') }}
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" id="langList">
+                    <li><a class="dropdown-item" href="{{ url('locale/en') }}"><img
+                                src=" {{ url('/images/lang/gb.svg') }}" alt="" style="max-height: 10px ">
+                            {{ __('English') }}</a>
+                    </li>
+                    <li><a class="dropdown-item" href="{{ url('locale/es') }}"><img
+                                src=" {{ url('/images/lang/es.svg') }}" alt="" style="max-height: 10px ">
+                            {{ __('Spanish') }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="dropdown">
+                <a class="btn btn-secondary dropdown-toggle bg-info" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src=" {{ url('/images/shopping-cart.png') }}" alt="shopping-cart" style="max-height: 18px;">
+                    <span> ({{ Cart::getContent()->count() }})</span>
+                </a>
+                <ul class="dropdown-menu shopping-cart" aria-labelledby="dropdownMenuLink" id="langList">
+                    @if (Cart::getContent())
+                        <li><a class="dropdown-item"
+                                href="{{ route('cart.checkout') }}">{{ __('View all cart') }}<span>
+                                    ({{ Cart::getContent()->count() }})</span></a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-light p-4 border-dark text-center d-block d-sm-block d-md-none">
+        <a class="nav-link border-bottom {{ setActive('home') }}" href={{ route('home') }}>
+            {{ __('Home') }}
+        </a>
+        <a class="nav-link border-bottom {{ setActive('shop') }}" href={{ route('shop') }}>
+            {{ __('Shop') }}
+        </a>
+        <a class=" nav-link border-bottom {{ setActive('booking') }}" href={{ route('booking') }}>
+            {{ __('Booking') }}
+        </a>
+        @guest
+            <a class="nav-link border-bottom {{ setActive('register') }}"
+                href={{ route('register') }}>{{ __('Register') }}</a>
+            <a class="nav-link {{ setActive('login') }}" href={{ route('login') }}>
+                {{ __('Login') }}
+            </a>
+        @else
+            @if (auth()->user()->type == 'admin')
+                <a class="nav-link border-bottom" href={{ route('dashboard') }}>{{ __('Manage') }}</a>
+            @else
+                <a class="nav-link border-bottom" href={{ route('dashboard') }}>{{ __('My account') }}</a>
+            @endif
+            <a class="nav-link" href="#"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        @endguest
+    </div>
+</div>
